9dc0b3d9f3c7ba4257525ee08839d6e8249e55da
Randrita Sarkar <randritas@gmail.com>
2025-07-28 19:31:19 +0000

    Give coral pink 1 px shadow to all the cards

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm hover:shadow-[0_0_0_1px_hsl(var(--secondary))]",
       className
     )}
     {...props}
